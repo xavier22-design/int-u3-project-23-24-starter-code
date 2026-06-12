47c91bd8d7a0c8597d77034383b4defbba9370e8
xavier22-design <doughtyxschool@gmail.com>
2026-06-12 15:30:03 -0400

    help

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
-
-
-
-
+let storyOpening = document.querySelector('.story-opening');
+let optionOneScreen = document.querySelector('.option-one-screen');
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let optionOneEnd = document.querySelector('.option-one-end');
+let optionTwoEnd = document.querySelector('.option-two-end');
+let optionOneEnd2 = document.querySelector('.option-one-end2');
+let optionTwoEnd2 = document.querySelector('.option-two-end2');
+let button_open_1 = document.querySelector('.open-option-one');
+let button_open_2 = document.querySelector('.open-option-two');
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+button_open_1.addEventListener('click', function(){
+    storyOpening.style.display = 'none';
+    optionOneScreen.style.display = 'block';
+ });
 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
